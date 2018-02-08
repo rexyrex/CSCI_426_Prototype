07a6052ec83de218e8d967bb75e8f0af7813dabc
Jeremy Nuttall <jtnuttal@usc.edu>
2018-02-07 20:29:26 -0800

    refactor playermovement to inherit

---
+++ Assets/Scripts/Player/PlayerMovements/Player2Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2Movement : PlayerMovement {
+
+	// Use this for initialization
+	public override void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	public override void Update () {
+		base.Update ();
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			transform.Translate (Vector3.back * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			transform.Translate (Vector3.left * speed * Time.deltaTime);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			transform.Translate (Vector3.right * speed * Time.deltaTime);
+		}
+	}
+}
--- Assets/Scripts/Player/PlayerMovements/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovements/PlayerMovement.cs
@@ -6,43 +6,12 @@ using UnityEngine.AI;
 [System.Serializable]
 public class PlayerMovement : MonoBehaviour {
 	[SerializeField]
-	float speed = 3.5f;
+	protected float speed = 3.5f;
 
-	[SerializeField]
-	public Keys keys;
-	public enum Keys { WASD, ARROWS };
-	KeyCode up, down, left, right;
-
-	void Start () {
-		if (keys == Keys.WASD) {
-			up = KeyCode.W;
-			down = KeyCode.S;
-			left = KeyCode.A;
-			right = KeyCode.D;
-		} else if (keys == Keys.ARROWS) {
-			up = KeyCode.UpArrow;
-			down = KeyCode.DownArrow;
-			left = KeyCode.LeftArrow;
-			right = KeyCode.RightArrow;
-		}
+	public virtual void Start () {
 	}
 
 
-	void Update () {
-		if (Input.GetKey (up)) {
-			transform.Translate (Vector3.forward * speed * Time.deltaTime);
-		}
-
-		if (Input.GetKey (down)) {
-			transform.Translate (Vector3.back * speed * Time.deltaTime);
-		}
-
-		if (Input.GetKey (left)) {
-			transform.Translate (Vector3.left * speed * Time.deltaTime);
-		}
-
-		if (Input.GetKey (right)) {
-			transform.Translate (Vector3.right * speed * Time.deltaTime);
-		}
+	public virtual void Update () {
 	}
 }
