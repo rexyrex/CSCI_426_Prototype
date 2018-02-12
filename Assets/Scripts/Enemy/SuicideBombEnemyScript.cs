262aaf0b42093ea6e2f8f77ee5b179e2fd265135
rexyrex <minhyung.kim1234@gmail.com>
2018-02-11 17:32:30 -0800

    Explosion added as particle effect + explosion enemy added
    Explosion doesnt do damage to player yet

---
+++ Assets/Scripts/Enemy/SuicideBombEnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuicideBombEnemyScript : MonoBehaviour {
+
+    private float explodeDelay = 10f;
+    private float timeInitialized = 0f;
+    //public ParticleSystem explosion;
+
+
+	void Start () {
+        timeInitialized = Time.time;
+	}
+	
+
+	void Update () {
+		if(Time.time - timeInitialized > explodeDelay)
+        {
+            var exp = gameObject.transform.GetChild(0).GetComponent<ParticleSystem>();
+            exp.Play();
+            Destroy(gameObject, exp.main.duration);
+        }
+	}
+}
