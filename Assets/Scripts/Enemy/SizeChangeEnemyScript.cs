0f1377e93d62ff5e87605e927c6da52d0959ac9c
rexyrex <minhyung.kim1234@gmail.com>
2018-02-11 16:45:03 -0800

    Size Changing Enemy and Splitting Enemy Added

---
+++ Assets/Scripts/Enemy/SizeChangeEnemyScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChangeEnemyScript : MonoBehaviour {
+
+    private float fullHealth = 50;
+    private float health = 50;
+
+    private float timeTillRegenStart = 3f;
+    private float regenSpeed = 1f;
+    private float regenAmount = 10f;
+
+    private float lastHitTime;
+    private float hitFreq = 0.02f;
+
+	// Use this for initialization
+	void Start () {
+        lastHitTime = Time.time;
+        gameObject.transform.localScale = new Vector3(health / fullHealth + 0.5f, health / fullHealth + 0.5f, health / fullHealth + 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Size of enemy based on percentage of health
+        //gameObject.transform.localScale.Set(health / fullHealth, health / fullHealth, health / fullHealth);
+        //gameObject.transform.localScale = new Vector
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void OnHitByChain(float damage)
+    {
+        //Debug.Log("HIT " + (Time.time - lastHitTime));
+        if(Time.time - lastHitTime > hitFreq)
+        {
+            //Debug.Log("Health is now: " + health);
+            health -= damage;
+            lastHitTime = Time.time;
+            gameObject.transform.localScale = new Vector3(health / fullHealth+0.5f, health / fullHealth + 0.5f, health / fullHealth + 0.5f);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Enemy/SplittingEnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplittingEnemyScript : MonoBehaviour {
+
+    float splitFreq = 4f;
+    float lastSplit;
+    public GameObject clone;
+    Quaternion quat = new Quaternion(0, 0, 0, 0);
+
+    // Use this for initialization
+    void Start () {
+        lastSplit = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - lastSplit > splitFreq)
+        {
+            GameObject inst = Instantiate(clone, gameObject.transform.position, quat);
+            lastSplit = Time.time;
+        }
+	}
+}
