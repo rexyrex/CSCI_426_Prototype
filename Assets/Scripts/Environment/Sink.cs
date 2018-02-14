5131a4c13631b198f897f30c7bcc297f0e5bfc23
matt-giles <gilesml@gmail.com>
2018-02-13 16:36:04 -0800

    changed wall to phase out

--- Assets/Scripts/Environment/Sink.cs
+++ Assets/Scripts/Environment/Sink.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Sink : EnvironmentObject {
     protected Vector3 movement;
+    protected BoxCollider bc;
+    protected Renderer mat;
+    protected Color activeCol;
+    protected Color fadeCol;
 
 	// Use this for initialization
 	void Start () {
-        movement = new Vector3(0, this.transform.position.y*2+0.1f, 0);
-	}
+        //movement = new Vector3(0, this.transform.position.y*2+0.1f, 0);
+        bc = this.GetComponent<BoxCollider>();
+        mat = this.GetComponent<Renderer>();
+        activeCol = mat.material.color;
+        fadeCol = new Color(activeCol.r, activeCol.g, activeCol.b, 0.5f);
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -17,11 +25,15 @@ public class Sink : EnvironmentObject {
 
     public override void Actuate()
     {
-        this.transform.Translate(movement*-1);
+        //this.transform.Translate(movement*-1);
+        mat.material.color = fadeCol;
+        bc.enabled = false;
     }
 
     public override void Revert()
     {
-        this.transform.Translate(movement);
+        //this.transform.Translate(movement);
+        mat.material.color = activeCol;
+        bc.enabled = false;
     }
 }
