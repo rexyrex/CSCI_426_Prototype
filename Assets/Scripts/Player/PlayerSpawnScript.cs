79ec9b5303de096481548dff5e79953ef0a55add
rexyrex <minhyung.kim1234@gmail.com>
2018-03-07 13:18:47 -0800

    players teleport to desired loc after chain creation

---
+++ Assets/Scripts/Player/PlayerSpawnScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawnScript : MonoBehaviour {
+
+	public Transform spawnPoint;
+	float spawnDelay = 1;
+	float elapsed;
+	bool posSet;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<Rigidbody> ().useGravity = false;
+		elapsed = 0;
+		posSet = false;
+	}
+
+	void Awake(){
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+		if (elapsed >= spawnDelay && posSet == false) {
+			posSet = true;
+			setPos();
+		}
+	}
+
+	void setPos(){
+		gameObject.GetComponent<Rigidbody> ().useGravity = true;
+		gameObject.transform.position = spawnPoint.position;
+	}
+}
