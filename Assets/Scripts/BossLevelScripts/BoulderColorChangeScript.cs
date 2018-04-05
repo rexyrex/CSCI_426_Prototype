55b8a7e1d063d0fcf94a1b32b822b0c754d1711b
rexyrex <minhyung.kim1234@gmail.com>
2018-04-04 20:50:59 -0700

    Adrian Boss Battle scene added
    Boulder color changer, new boulders. Need to implement boss changing
    color, add boss hp, make boulders able to damage boss

---
+++ Assets/Scripts/BossLevelScripts/BoulderColorChangeScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderColorChangeScript : MonoBehaviour {
+	public enum boulderMode {Close, Medium, Far}
+
+	boulderMode mode;
+
+	public Material closeMat;
+	public Material medMat;
+	public Material farMat;
+	public Material invincibleMat;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mode = (boulderMode)GlobalDataController.gdc.boulderState;
+		updateMaterial ();
+	}
+
+	void updateMaterial(){
+		switch (mode) {
+		case boulderMode.Close:
+			GetComponent<Renderer> ().material = closeMat;
+			break;
+		case boulderMode.Medium:
+			GetComponent<Renderer> ().material = medMat;
+			break;
+		case boulderMode.Far:
+			GetComponent<Renderer> ().material = farMat;
+			break;
+		default:
+			GetComponent<Renderer> ().material = invincibleMat;
+			break;
+		}
+	}
+}
---
+++ Assets/Scripts/BossLevelScripts/ColorChangerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChangerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("COLLIDEDEDEDED");
+		if (other.tag == "Chain" && GlobalDataController.gdc.chainCharged) {
+			GlobalDataController.gdc.boulderState = GlobalDataController.gdc.chainState;
+		}			
+	}
+
+	void OnCollisionEnter(Collision other){
+		Debug.Log ("COLLIDEDEDEDED");
+		if (other.collider.tag == "Chain"&& GlobalDataController.gdc.chainCharged) {
+			GlobalDataController.gdc.boulderState = GlobalDataController.gdc.chainState;
+		}			
+	}
+}
