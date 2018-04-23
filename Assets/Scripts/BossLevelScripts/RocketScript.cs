a15c18c32bb7f85129d3873b1c8b180a3ad3d7f1
rexyrex <minhyung.kim1234@gmail.com>
2018-04-22 19:36:37 -0700

    Boss Level Updated 2
    Things todo:
    1. Enemies need to explode on impact again
    2. Generic Enemy script needs die, die by chain methods and a timer for
    how long they remain alive before they die off
    3. Fix rocket sinking into ground
    4. Have a look at why sometimes boulder does not explode on boss
    5. Enemies should recover after a few seoncds after they've been hit by
    chain.
    6. Make clouds semitransparent
    7. Change color of rocket spawner
    8. Boss should be more threatening (ranged attack?)

---
+++ Assets/Scripts/BossLevelScripts/RocketScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketScript : MonoBehaviour {
+
+	Transform target;
+	public float speed;
+	public GameObject explosion;
+
+	void Start(){
+		GameObject bossobj = GameObject.FindGameObjectWithTag ("Boss");
+		target = bossobj.transform;
+	}
+
+	void Update() {
+		float step = speed * Time.deltaTime;
+		transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+	}
+
+	void OnTriggerEnter(Collider other){
+		//Debug.Log ("COLLIDEDEDEDED");
+		if (other.tag == "Boss" ) {
+			Vector3 pos = gameObject.transform.position;
+			Quaternion quat = new Quaternion(0, 0, 0, 0);
+			Instantiate (explosion, pos, quat);
+			Destroy (gameObject);
+			//Debug.Log ("ouch");
+		}	
+	}
+		
+
+
+}
---
+++ Assets/Scripts/BossLevelScripts/RocketSpawnerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketSpawnerScript : MonoBehaviour {
+
+	GameObject p1;
+	GameObject p2;
+	bool p1Inside;
+	bool p2Inside;
+
+	public GameObject rocket;
+
+	float scaleIncFreq = 2f;
+	float scaleLastInc;
+
+	// Use this for initialization
+	void Start () {
+		p1 = GameObject.FindGameObjectWithTag ("Player1Tag");
+		p2 = GameObject.FindGameObjectWithTag ("Player2Tag");
+		p1Inside = false;
+		p2Inside = false;
+		scaleLastInc = Time.time;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other){
+		//Debug.Log ("COLLIDEDEDEDED");
+		if (other.tag == "Player1Tag" ||  other.tag == "Player2Tag") {
+			FireRocket ();
+
+		}
+
+		if (other.tag == "Enemy" ) {
+			other.gameObject.GetComponent<BasicEnemyScript> ().OnHitByChain (1000, true);
+			FireRocket ();
+
+		}	
+	}
+
+	void FireRocket(){
+		Vector3 pos = gameObject.transform.position;
+		Quaternion quat = new Quaternion(0, 0, 0, 0);
+		Instantiate (rocket, pos, quat);
+	}
+
+	void OnTriggerExit(Collider other){
+		//Debug.Log ("COLLIDEDEDEDED");
+		if (other.tag == "Player1Tag" ) {
+			p1Inside = false;
+			//Debug.Log ("ouch");
+		}
+
+		if (other.tag == "Player2Tag" ) {
+			p2Inside = false;
+		}	
+	}
+}
