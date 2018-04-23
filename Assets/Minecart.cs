8ef9e1daf6e5ebe325cdc94f4cc372baa56db5cf
matt-giles <gilesml@gmail.com>
2018-04-22 20:53:31 -0700

    minecart fix

--- Assets/Minecart.cs
+++ Assets/Minecart.cs
@@ -4,92 +4,82 @@ using UnityEngine;
 
 public class Minecart : MonoBehaviour {
     Transform parentTrans;
-    public float max;
-    public float min;
     public float axis;
     public Rail startRail;
-    Vector3 localpos;
-    Quaternion rotation;
-    Quaternion localRot;
-    float startPos;
-    float startY;
+    Quaternion ZRotation;
+    Quaternion XRotation;
     List<Rail> rails;
+    Rigidbody rb;
+    float trackMass;
+    float derailMass;
 
 	// Use this for initialization
 	void Start () {
+        XRotation = new Quaternion(0, 0, 0, 0);
+        ZRotation = new Quaternion(0, 90, 0, 0);
+        trackMass = 4;
+        derailMass = 500;
+
         parentTrans = this.GetComponentInParent<Transform>();
-        float rot = parentTrans.rotation.y;
-        startY = parentTrans.position.y;
+
+        rb = this.GetComponent<Rigidbody>();
+
         rails = new List<Rail>();
-        localpos = this.transform.position;
-        if(startRail!=null) rails.Add(startRail);
-        if (axis == 1)
-        {
-            startPos = parentTrans.position.x;
-            parentTrans.rotation.Set(0, 90, 0, 0);
-        }
-        else if (axis == 0)
-        {
-            startPos = parentTrans.position.z;
-            parentTrans.rotation.Set(0, 0, 0, 0);
-        }
-        else axis = -1;
+        if (startRail != null) AddRail(startRail);
 
-        localRot = this.transform.rotation;
-        rotation = parentTrans.rotation;
-
+        SetConstraints();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (rails.Count == 1)
-        {
-            axis = rails[0].axis;
-            if (axis == 1)
-            {
-                startPos = parentTrans.position.x;
-                rotation = new Quaternion(0, 90, 0, 0);
-                parentTrans.rotation = rotation;
-            }
-            else if (axis == 0)
-            {
-                startPos = parentTrans.position.z;
-                rotation = new Quaternion(0, 0, 0, 0);
-                parentTrans.rotation = rotation;
-            }
-        }
-
-        else return;   
-        if (axis == 1)
-        {
-            float x = startPos;
-            float y = startY;
-            float z = parentTrans.position.z;
-            if (z > max) z = max;
-            else if (z < min) z = min;
-            parentTrans.position.Set(x, y, z);
-        }
-        else if (axis == 0)
-        {
-            float x = startPos;
-            float y = startY;
-            float z = parentTrans.position.z;
-            if (z > max) z = max;
-            else if (z < min) z = min;
-            parentTrans.position.Set(x, y, z);
-        }
-        this.transform.position = localpos;
-        parentTrans.rotation = rotation;
-        this.transform.rotation = localRot;
+        if (rails.Count == 1) SetConstraints(); // Might need changing
+        else return; // Don't Change anything
     }
 
+    // Found a new rail
     public void AddRail(Rail item)
     {
+        if (rails.Count < 1) rb.mass = trackMass;
         rails.Add(item);
     }
 
+    // Moved off of a rail
     public void RemoveRail(Rail item)
     {
         rails.Remove(item);
+        if (rails.Count < 1) rb.mass = derailMass;
     }
+
+    // Sets constraints
+    void SetConstraints()
+    {
+        axis = rails[0].axis;
+        if (axis == 1)
+        {
+            FreezeX();
+        }
+        else if (axis == 0)
+        {
+            FreezeZ();
+        }
+    }
+
+    // Let's the cart only move in Z
+    void FreezeX()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        rb.rotation = ZRotation;
+        rb.freezeRotation = true;
+        rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY;
+    }
+
+    // Let's the cart only move in X
+    void FreezeZ()
+    {
+        rb.constraints = RigidbodyConstraints.None;
+        rb.rotation = XRotation;
+        rb.freezeRotation = true;
+        rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionY;
+    }
+
 }
